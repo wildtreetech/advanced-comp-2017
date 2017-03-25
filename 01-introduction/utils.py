87afee8429758d164232127ad1e2e57ac7641074
Tim Head <betatim@gmail.com>
2017-03-25 17:55:15 +0100

    Start first lecture notes

---
+++ 01-introduction/utils.py
@@ -0,0 +1,85 @@
+import subprocess
+from tempfile import mkstemp
+from itertools import cycle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy import interp
+
+from sklearn.tree import export_graphviz
+from sklearn.metrics import roc_curve, auc
+
+from IPython.core.display import HTML
+
+
+def plot_surface(clf, X, y, n_steps=250, subplot=None, show=True):
+    if subplot is None:
+        fig = plt.figure()
+    else:
+        plt.subplot(*subplot)
+
+    xlim = X[:, 0].min(), X[:, 0].max()
+    ylim = X[:, 1].min(), X[:, 1].max()
+    xx, yy = np.meshgrid(np.linspace(xlim[0], xlim[1], n_steps),
+                         np.linspace(ylim[0], ylim[1], n_steps))
+
+    if hasattr(clf, "decision_function"):
+        z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+    z = z.reshape(xx.shape)
+    plt.contourf(xx, yy, z, alpha=0.8, cmap=plt.cm.RdBu_r)
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.xlim(*xlim)
+    plt.ylim(*ylim)
+
+    if show:
+        plt.show()
+
+
+def draw_tree(clf, feature_names, **kwargs):
+    _, name = mkstemp(suffix='.dot')
+    _, svg_name = mkstemp(suffix='.svg')
+    export_graphviz(clf, out_file=name,
+                    feature_names=feature_names,
+                    **kwargs)
+    command = ["dot", "-Tsvg", name, "-o", svg_name]
+    subprocess.check_call(command)
+    return HTML(open(svg_name).read())
+
+
+# Taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html
+def draw_roc_curve(classifier, cv, X, y):
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+
+    colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
+    lw = 2
+
+    for i, (train, test) in enumerate(cv):
+        probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=lw, color='k',
+             label='Luck')
+
+    mean_tpr /= len(cv)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, color='g', linestyle='--',
+             label='Mean ROC (area = %0.2f)' % mean_auc, lw=lw)
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# advanced-comp-2017
-💻 Material for a course on applied machine-learning for scientists at EPFL. Spring 2017
+# Applied machine-learning for science
+
+💻 Material for a course on applied machine-learning for scientists taught at
+EPFL in Spring 2017.
+
+🚧 This is still under heavy construction 🚧
+
+# Outline
+
+The course consists of six two hour lectures, followed by one hour to discuss
+the weeks homework assignment.
+
+Current bucket list of topics to cover ((~) denotes: short introduction to):
+
+1. General problem statement and introduction
+2. Ensembles of trees: forests and gradient boosting
+3. Neural networks: convolutions aren't convoluted
+4. Model selection and evaluation: predict future performance
+5. PCA and t-SNE: lower dimensional embeddings and visualisation
+5. Bayesian optimisation for hyper-parameter tuning (~)
+6. Meet a GAN: cops and robbers for neural networks (~)
+7. Fine tuning state the state of the art: standing on the shoulder of giants (~)
+8. Probabilistic datastructures: a bonus lecture
+
+
+# Technicalities, installing, running code
+
+All the code will be written in python. We will make use of the scientific
+python stack:
+
+* python v3.6
+* numpy v1.12.1
+* scikit-learn v0.18.1
+* keras
+* matplotlib v2.0.0
+* jupyter v5.0.0
+
+All work submitted for credit has to run with those dependencies only.
+
+... instructions for setting things up on your laptop ...
+
+# License
+
+Heavily inspired by ESL, ISL, Introduction to scikit-learn, and lecture notes
+by Gilles Louppe.
+
+All original work is licensed under [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
